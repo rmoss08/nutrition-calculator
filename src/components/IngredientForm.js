15372429a3ef7b1b66164a839f4f07543aa3afe6
Robert J. Moss <robert.moss3409@gmail.com>
2022-06-20 09:30:05 -0400

    refactor: calculates weighted nutrition values in IngredientForm instead of Table

--- src/components/IngredientForm.js
+++ src/components/IngredientForm.js
@@ -3,27 +3,55 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { mealActions } from '../store/meal-slice';
 
+const TEST_CUCUMBER = {
+  sugar_g: 0.85,
+  fiber_g: 0.25,
+  sodium_mg: 0.5,
+  potassium_mg: 12,
+  fat_saturated_g: 0,
+  fat_total_g: 0.05,
+  calories: 7.65,
+  cholesterol_mg: 0,
+  protein_g: 0.3,
+  carbohydrates_total_g: 1.85,
+};
+
 const IngredientForm = () => {
   const dispatch = useDispatch();
 
-  const formatNutritionData = (data) => {
-    delete data.name;
+  const calculateWeightedValues = (weight, object) => {
+    const nutritionServingSize = object.serving_size_g;
+    const servingSizeFactor = weight / nutritionServingSize;
 
+    let weightedNutritionValues = {};
+
+    for (const key in object) {
+      if (key !== 'serving_size_g') {
+        const nutritionValue = object[key];
+        const weightedValue = nutritionValue * servingSizeFactor;
+        weightedNutritionValues[key] = weightedValue;
+      }
+    }
+
+    return weightedNutritionValues;
+  };
+
+  const convertToFloat = (object) => {
     let formattedData = {};
-    for (const key in data) {
-      formattedData[key] = parseFloat(data[key]);
+    for (const key in object) {
+      formattedData[key] = parseFloat(object[key]);
     }
 
     return formattedData;
   };
 
-  const fetchNutritionData = (ingredient, weight, timestamp) => {
+  const fetchNutritionData = (ingredientName) => {
     const axios = require('axios');
 
     const options = {
       method: 'GET',
       url: 'https://calorieninjas.p.rapidapi.com/v1/nutrition',
-      params: { query: ingredient },
+      params: { query: ingredientName },
       headers: {
         'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com',
         'X-RapidAPI-Key': 'f31dca7789msh9b76c7ca9ee7a84p1aaa5cjsn75b70701b51a',
@@ -34,18 +62,8 @@ const IngredientForm = () => {
       .request(options)
       .then((response) => {
         const data = response.data.items[0];
-        // console.log(data);
 
-        const formattedData = formatNutritionData(data);
-
-        const ingredientData = {
-          id: timestamp,
-          name: ingredient,
-          weight: weight,
-          nutrition: formattedData,
-        };
-
-        return dispatch(mealActions.add(ingredientData));
+        return data;
       })
       .catch((error) => {
         console.error(error);
@@ -55,11 +73,30 @@ const IngredientForm = () => {
   const submitHandler = async (event) => {
     event.preventDefault();
 
-    const ingredient = event.target[0].value;
+    const ingredientName = event.target[0].value;
     const weight = parseFloat(event.target[1].value);
-    const timestamp = `${event.timeStamp}`;
 
-    return fetchNutritionData(ingredient, weight, timestamp);
+    // --- Commented out for testing purpose
+    // fetchNutritionData(ingredientName).then((nutritionData) => {
+    //   delete nutritionData.name;
+
+    //   const floatNutritionData = convertToFloat(nutritionData);
+    //   const weightedNutritionData = calculateWeightedValues(
+    //     weight,
+    //     floatNutritionData
+    //   );
+
+    const ingredient = {
+      name: ingredientName,
+      weight: parseFloat(event.target[1].value),
+      id: `${event.timeStamp}`,
+      nutrition: TEST_CUCUMBER,
+      // --- Commented out for testing purpose
+      // --- NOTE: ingredient and return statement should go back in .then()
+      // nutrition: weightedNutritionData,
+    };
+
+    return dispatch(mealActions.add(ingredient));
   };
 
   return (
