069f9923e5afa5b89eea37916ad4024524c35bb1
Robert J. Moss <robert.moss3409@gmail.com>
2022-06-27 10:34:02 -0400

    refactor: replaces several methods with Row and Total components

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,7 @@
-import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { dayActions } from '../store/day-slice';
 import { mealActions } from '../store/meal-slice';
 import Total from './Total';
+import Row from './Row';
 
 const TABLE_HEADER = [
   'rowData',
@@ -22,142 +21,37 @@ const TABLE_HEADER = [
 
 const Table = (props) => {
   const dispatch = useDispatch();
-
-  const ingredients = useSelector((state) => state.meal.ingredients);
-  const selectedMeals = useSelector((state) => state.day.selectedMeals);
-  const mealTotal =useSelector((state) => state.meal.mealTotal);
-
-  let tableData;
-  let mealTable;
-  if (props.slice === 'meal') {
-    tableData = ingredients;
-    mealTable = true;
-  } else if (props.slice === 'day') {
-    tableData = selectedMeals;
-    mealTable = false;
-  } else {
-    tableData = null;
-    console.log('Invalid slice props');
-  }
-
-  const thElements = TABLE_HEADER.map((description) => (
-    <th key={description}>{description}</th>
-  ));
-
-  const createRowElement = (rowData) => {
-    let tdElements = [
-      <td key="name">{rowData.name}</td>,
-      <td key="weight">{mealTable ? rowData.weight : ''}</td>,
-      <td key="action">
-        <button>+</button>
-        <button>-</button>
-      </td>,
-    ];
-
-    const nutrition = rowData.nutrition;
-    for (const key in nutrition) {
-      tdElements.push(<td key={key}>{nutrition[key]}</td>);
-    }
-
-    return <tr key={rowData.id}>{tdElements}</tr>;
-  };
-
-  const createTotalRowElement = (totals) => {
-    let tdElements = [
-      <td key="total">Total</td>,
-      <td key="weight"></td>,
-      <td key="action"></td>,
-    ];
-
-    for (const key in totals) {
-      tdElements.push(<td key={key}>{totals[key]}</td>);
-    }
-
-    return <tr key={totals}>{tdElements}</tr>;
-  };
-
-  let totals = {
-    sugar_g: 0,
-    fiber_g: 0,
-    sodium_mg: 0,
-    potassium_mg: 0,
-    fat_saturated_g: 0,
-    fat_total_g: 0,
-    calories: 0,
-    cholesterol_mg: 0,
-    protein_g: 0,
-    carbohydrates_total_g: 0,
-  };
-
-  const calculateTotals = (tableData) => {
-    for (const i in tableData) {
-      const rowData = tableData[i];
-      for (const key in rowData.nutrition) {
-        const prevTotal = totals[key];
-        totals[key] = prevTotal + rowData.nutrition[key];
-      }
-    }
-    return totals;
-  };
-
-  const addMealHandler = (event) => {
-    event.preventDefault();
-    let mealNameInput = document.getElementById('meal-name');
-
-    const meal = { name: mealNameInput.value, nutrition: mealTotal };
-
-    if (tableData.length > 0) {
-      dispatch(dayActions.addToMeals(meal));
-      dispatch(mealActions.reset());
-      mealNameInput.value = '';
-    }
-  };
+  const tableData = useSelector((state) => state.meal.ingredients);
 
   const resetHandler = (event) => {
     event.preventDefault();
 
-    return mealTable
-      ? dispatch(mealActions.reset())
-      : dispatch(dayActions.resetSelectedMeals());
+    return dispatch(mealActions.reset());
   };
-
+  
+  
+  const thElements = TABLE_HEADER.map((description) => (
+    <th key={description}>{description}</th>
+  ));
+  
   let tbodyElements = [];
-
   if (tableData.length > 0) {
-    tbodyElements = tableData.map((rowData) => {
-      return createRowElement(rowData);
-    });
-    
-    let totalRowElement;
-    if (props.slice === 'day') {
-      totalRowElement = <Total tableData={tableData} slice='day' />
-    } else {
-      totalRowElement = <Total tableData={tableData} />
-    }
-    // totals = calculateTotals(tableData);
-    // if (props.slice === 'day') {
-    //   dispatch(dayActions.updateTotals(totals));
-    // }
-
-    // const totalRowElement = createTotalRowElement(totals);
-    tbodyElements.push(totalRowElement);
+    tbodyElements = tableData.map((rowData) => <Row tdData={rowData} />);
+    tbodyElements.push(<Total tableData={tableData} />);
   }
 
   return (
     <div>
-      {mealTable && (
-        <div>
+        {/* <div>
           <label htmlFor="meal-name">Meal Name:</label>
           <input type="text" id="meal-name"></input>
-        </div>
-      )}
+        </div> */}
       <table>
         <thead>
           <tr>{thElements}</tr>
         </thead>
         <tbody>{tbodyElements}</tbody>
       </table>
-      {mealTable && <button onClick={addMealHandler}>Add Meal</button>}
       <button onClick={resetHandler}>Reset</button>
     </div>
   );
