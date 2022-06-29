436a9dbebd3a36a0eab22d1f8c08eacd06dd80b5
Robert J. Moss <robert.moss3409@gmail.com>
2022-06-29 14:19:02 -0400

    feat: adds RemoveIngredientButton component

---
+++ src/components/RemoveIngredientButton.js
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { mealActions } from "../store/meal-slice";
+
+const RemoveIngredientButton = (props) => {
+  const dispatch = useDispatch();
+  
+    const clickHandler = (event) => {
+    event.preventDefault();
+
+    console.log(props.ingredientId);
+    return dispatch(mealActions.remove(props.ingredientId));
+  };
+  
+    return (
+    <button onClick={clickHandler}>
+      <span className="material-symbols-outlined">remove</span>
+    </button>
+  );
+};
+
+export default RemoveIngredientButton;
