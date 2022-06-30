9f04c81950137e7ca65d6cd50591bc25eb370b57
Robert J. Moss <robert.moss3409@gmail.com>
2022-06-30 13:07:59 -0400

    refactor: creates Layout component from nav and footer elements on App

---
+++ src/components/Layout.js
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+
+const Layout = (props) => {
+  return (
+    <Fragment>
+      <nav>
+        <h1>NutriCalc</h1>
+      </nav>
+      <section>
+        {props.children}
+      </section>
+      <footer>
+        <p className="footer-credit">
+          Created by{' '}
+          <a
+            href="https://www.linkedin.com/in/robertjmoss/"
+            className="linkedin-hyperlink"
+          >
+            Robert J. Moss
+          </a>
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Layout;
