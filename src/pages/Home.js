e2f09ca1e2725ef74a1e19e60b17d86f2a1f3d41
Robert J. Moss <robert.moss3409@gmail.com>
2022-07-01 09:36:43 -0400

    feat: adds Home and NotFound page components
    The Home component is the original App refactored

---
+++ src/pages/Home.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import BarChart from '../components/BarChart';
+import IngredientForm from '../components/IngredientForm';
+import Table from '../components/Table';
+import { mealActions } from '../store/meal-slice';
+import { Fragment } from 'react';
+import Layout from '../components/Layout';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const [showBarChart, setShowBarChart] = useState(false);
+
+  const resetTableClickHandler = (event) => {
+    event.preventDefault();
+
+    setShowBarChart(false);
+    dispatch(mealActions.reset());
+  };
+
+  const showChartClickHandler = (event) => {
+    event.preventDefault();
+
+    return setShowBarChart(true);
+  };
+
+  const hideChartClickHandler = (event) => {
+    event.preventDefault();
+
+    return setShowBarChart(false);
+  };
+
+  return (
+    <Fragment>
+      <Layout>
+        <div className="wrapper">
+          <IngredientForm />
+          <br></br>
+          <Table slice="meal" />
+          <br></br>
+          <button onClick={resetTableClickHandler}>Reset</button>
+          <br></br>
+          {!showBarChart && (
+            <button onClick={showChartClickHandler}>
+              <span className="material-symbols-outlined">bar_chart</span>
+            </button>
+          )}
+          {showBarChart && (
+            <div>
+              <BarChart />
+              <button onClick={hideChartClickHandler}>Hide Chart</button>
+            </div>
+          )}
+        </div>
+      </Layout>
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const NotFound = () => {
+    return <Layout>
+        <div className="wrapper">
+            <h1>404 Error</h1>
+            <p>This page does not exist</p>
+        </div>
+    </Layout>
+};
+
+export default NotFound
