fc0df15535998c5923a3379945c7cc113bbd4d2e
Robert J. Moss <robert.moss3409@gmail.com>
2022-07-01 09:46:16 -0400

    feat: creates a LoadingSpinner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { lazy, Suspense, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import LoadingSpinner from './components/LoadingSpinner';
 
 const HomePage = lazy(() => import('./pages/Home'));
 const NotFoundPage = lazy(()=> import('./pages/NotFound'))
 
 function App() {
   return (
-    <Suspense>
+    <Suspense fallback={<LoadingSpinner />}>
       <Routes>
         <Route path="/" element={<HomePage />} exact />
         <Route path="*" element={<NotFoundPage />} />
---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import styles from './LoadingSpinner.module.css';
+
+const LoadingSpinner = () => {
+  return (
+    <div className={styles["lds-ring"]}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
