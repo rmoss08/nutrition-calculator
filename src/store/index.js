89979f57d08153bacb27605037cb7325dd9799dc
Robert J. Moss <robert.moss3409@gmail.com>
2022-07-11 11:22:59 -0400

    refactor: deletes MealForm and day-slice files
    Removes MealForm and day-slice because they are no longer necessary as the web app is no longer calculating daily totals. Also, the day reducer was removed from store/index for consistency

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mealReducer from './meal-slice.js';
-import dayReducer from './day-slice.js';
 
 const store = configureStore({
-    reducer: { meal: mealReducer, day: dayReducer },
+    reducer: { meal: mealReducer },
 });
 
 export default store;
