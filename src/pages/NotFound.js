80c25eae140ba5d8c4211ddbbcd9e2148f23ce38
Robert J. Moss <robert.moss3409@gmail.com>
2022-07-11 11:16:03 -0400

    styles: adds page-sub-section class

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,8 +3,10 @@ import Layout from "../components/Layout";
 const NotFound = () => {
     return <Layout>
         <div className="wrapper">
-            <h1>404 Error</h1>
-            <p>This page does not exist</p>
+            <div className="page-sub-section">
+                <h1>404 Error</h1>
+                <p>This page does not exist</p>
+            </div>
         </div>
     </Layout>
 };
