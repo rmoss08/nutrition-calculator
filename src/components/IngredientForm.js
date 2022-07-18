3245a9f5e0b5229556b1e6193a80f3cce533c6ca
Robert J. Moss <robert.moss3409@gmail.com>
2022-07-18 10:43:58 -0400

    refactor: removes old code, splits up resetForm, and changes variable names on IngredientForm
    Specifically, removes the old fetchNutritionData method, and splits up the resetForm method into resetInputValues and resetErrorStates

--- src/components/IngredientForm.js
+++ src/components/IngredientForm.js
@@ -5,24 +5,12 @@ import { mealActions } from '../store/meal-slice';
 import Error from './Error';
 import styles from './IngredientForm.module.css';
 
-const TEST_CUCUMBER = {
-  serving_size_g: 100,
-  sugar_g: 0.85,
-  fiber_g: 0.25,
-  sodium_mg: 0.5,
-  potassium_mg: 12,
-  fat_saturated_g: 0,
-  fat_total_g: 0.05,
-  calories: 7.65,
-  cholesterol_mg: 0,
-  protein_g: 0.3,
-  carbohydrates_total_g: 1.85,
-};
 const INGREDIENT_LIMIT = 50;
 
 export const calculateWeightedNutrition = (ingredient, newQuantity = false) => {
   const quantity = newQuantity ? newQuantity : ingredient.userQuantity_g;
   const servingSizeFactor = quantity / ingredient.apiServingSize_g;
+  
   let weightedNutrition = {};
   const apiNutrition = ingredient.apiNutrition;
 
@@ -41,14 +29,14 @@ const IngredientForm = () => {
   const [isInvalidIngredient, setIsInvalidIngredient] = useState(false);
   const [isIngredientLimitMet, setIsIngredientLimitMet] = useState(false);
   const [isAPIConnectionDown, setIsAPIConnectionDown] = useState(false);
-
+  
   const ingredientInputRef = useRef(null);
   const quantityInputRef = useRef(null);
 
   const dispatch = useDispatch();
   const ingredients = useSelector((state) => state.meal.ingredients);
   const numberOfIngredients = ingredients.length;
-
+  
   const closeErrorHandler = (type) => {
     if (type === 'invalidIngredient') {
       return setIsInvalidIngredient(false);
@@ -61,77 +49,53 @@ const IngredientForm = () => {
     }
   };
 
-  const convertObjectDataToNumbers = (objectToConvert) => {
-    let floatObject = {};
-
+  const convertObjectValuesToNumbers = (objectToConvert) => {
+    let objectWithNumberValues = {};
+    
     for (const key in objectToConvert) {
-      floatObject[key] = Number(objectToConvert[key]);
+      objectWithNumberValues[key] = Number(objectToConvert[key]);
     }
 
-    return floatObject;
+    return objectWithNumberValues;
   };
+  
+  const resetInputValues = () => {
+    ingredientInputRef.current.value = '';
+    quantityInputRef.current.value = '';
+  };
+  
+  const resetErrorStates = () => {
+    setIsInvalidIngredient(false);
+    setIsAPIConnectionDown(false);
+    setIsIngredientLimitMet(false);
+  }
 
   const fetchNutritionData = (ingredientName) => {
     const axios = require('axios');
-
+    
     const options = {
       method: 'GET',
       url: 'http://localhost:8000/fetch',
       params: { query: ingredientName },
     };
-
+    
     return axios
-      .request(options)
-      .then((response) => {
-        // console.log(response);
-        return response.data.items[0];
+    .request(options)
+    .then((response) => {
+      return response.data.items[0];
       })
       .catch((error) => {
         console.error(error);
-        setIsAPIConnectionDown(true);
+        return setIsAPIConnectionDown(true);
       });
   };
-  // const fetchNutritionData = (ingredientName) => {
-  //   const axios = require('axios');
 
-  //   const options = {
-  //     method: 'GET',
-  //     url: 'https://calorieninjas.p.rapidapi.com/v1/nutrition',
-  //     params: { query: ingredientName },
-  //     headers: {
-  //       'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com',
-  //       'X-RapidAPI-Key': 'f31dca7789msh9b76c7ca9ee7a84p1aaa5cjsn75b70701b51a',
-  //     },
-  //   };
-
-  //   return axios
-  //     .request(options)
-  //     .then((response) => {
-  //       const data = response.data.items[0];
-
-  //       return data;
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //       setIsAPIConnectionDown(true);
-  //     });
-  // };
-
-  const resetForm = () => {
-    ingredientInputRef.current.value = '';
-    quantityInputRef.current.value = '';
-
-    setIsInvalidIngredient(false);
-    setIsAPIConnectionDown(false);
-    setIsIngredientLimitMet(false);
-  };
 
   const submitHandler = async (event) => {
     event.preventDefault();
 
     const userIngredientName = event.target[0].value;
 
-    // --- Comment out for testing purpose
     fetchNutritionData(userIngredientName).then((nutritionData) => {
       try {
         if (nutritionData === undefined || null) {
@@ -140,23 +104,24 @@ const IngredientForm = () => {
           const apiIngredientName = nutritionData.name;
           delete nutritionData.name;
 
-          const floatNutrition = convertObjectDataToNumbers(nutritionData);
+          const formattedNutritionData = convertObjectValuesToNumbers(nutritionData);
 
-          const apiServingSize_g = floatNutrition['serving_size_g'];
-          delete floatNutrition['serving_size_g'];
+          const apiServingSize_g = formattedNutritionData['serving_size_g'];
+          delete formattedNutritionData['serving_size_g'];
 
           const ingredient = {
             id: `${Number(event.timeStamp)}`,
             name: apiIngredientName,
             userQuantity_g: parseFloat(event.target[1].value),
             apiServingSize_g,
-            apiNutrition: floatNutrition,
+            apiNutrition: formattedNutritionData,
           };
 
           const userNutrition = calculateWeightedNutrition(ingredient);
           ingredient['userNutrition'] = userNutrition;
 
-          resetForm();
+          resetInputValues();
+          resetErrorStates();
 
           return dispatch(mealActions.add(ingredient));
         }
