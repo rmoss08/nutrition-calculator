13112f67952599d6a281278b4f8e1915b12ae6a5
Robert J. Moss <robert.moss3409@gmail.com>
2022-07-19 09:42:19 -0400

    style: adds styling to ServingForm

--- src/components/ServingForm.js
+++ src/components/ServingForm.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { mealActions } from '../store/meal-slice';
+import styles from './ServingForm.module.css';
 
 const ServingForm = () => {
   const dispatch = useDispatch();
@@ -13,11 +14,21 @@ const ServingForm = () => {
 
   return (
     <form onSubmit={submitHandler}>
-      <div>
-        <label htmlFor="serving-input">Serving Size:</label>
-        <input id="serving-input" type="number" min={1} placeholder={1} />
+      <div className={styles['serving-form__grid']}>
+        <div className="general-form__field">
+          <label htmlFor="serving-input" className="general-form__field-label">
+            Serving Size:
+          </label>
+          <input
+            id="serving-input"
+            className="general-form__field-input"
+            type="number"
+            min={1}
+            placeholder={1}
+          />
+        </div>
       </div>
-      <button>Update</button>
+      <button className="page-subsection__button">Update</button>
     </form>
   );
 };
