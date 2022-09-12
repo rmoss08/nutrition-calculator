b43e63489562f77baf03ed828af5600c2463819c
Robert J. Moss <robert.moss3409@gmail.com>
2022-09-12 15:21:07 -0400

    bug: fixes eslint problems

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,36 @@
-const functions = require('firebase-functions');
-const axios = require('axios');
+const functions = require("firebase-functions");
+const axios = require("axios");
 
 // The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin
-    .firestore()
-    .collection('messages')
-    .add({ original: original });
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
 });
 
 exports.fetchNutrition = functions.https.onRequest((req, res) => {
   const ingredient = req.query.ingredient;
   const options = {
-    method: 'GET',
-    url: 'https://calorieninjas.p.rapidapi.com/v1/nutrition',
-    params: { query: ingredient },
+    method: "GET",
+    url: "https://calorieninjas.p.rapidapi.com/v1/nutrition",
+    params: {query: ingredient},
     headers: {
-      'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com",
+      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     },
   };
 
   return axios
-    .request(options)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+      .request(options)
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 });
