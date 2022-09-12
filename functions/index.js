068d0f9b679835cdf260e2093e00ed2c53fd7756
Robert J. Moss <robert.moss3409@gmail.com>
2022-09-12 13:19:23 -0400

    feat: adds a new Firebase function, fetchNutrition

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const axios = require('axios');
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
@@ -8,18 +9,42 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
 });
 
-
-// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
 exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
   // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({original: original});
+  const writeResult = await admin
+    .firestore()
+    .collection('messages')
+    .add({ original: original });
   // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+exports.fetchNutrition = functions.https.onRequest((req, res) => {
+  const ingredient = req.query.ingredient;
+  const options = {
+    method: 'GET',
+    url: 'https://calorieninjas.p.rapidapi.com/v1/nutrition',
+    params: { query: ingredient },
+    headers: {
+      'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+
+  return axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
