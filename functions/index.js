362e6ab22bad71399a831d3ff7bacbef90d38bbd
Robert J. Moss <robert.moss3409@gmail.com>
2022-09-13 16:19:27 -0400

    feat: adds Firebase function, fetchAPINutrition

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,31 @@
-const functions = require("firebase-functions");
-const axios = require("axios");
+const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to access Firestore.
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-
-exports.fetchNutrition = functions.https.onRequest((req, res) => {
-  const ingredient = req.query.ingredient;
+exports.fetchAPINutrition = functions.https.onRequest((req, res) => {
+  const axios = require('axios');
+  
+  const ingredient = req.query.ingredient;  
   const options = {
-    method: "GET",
-    url: "https://calorieninjas.p.rapidapi.com/v1/nutrition",
-    params: {query: ingredient},
+    method: 'GET',
+    url: 'https://calorieninjas.p.rapidapi.com/v1/nutrition',
+    params: { query: ingredient },
     headers: {
-      "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     },
   };
+  
+  res.set('Access-Control-Allow-Origin', '*');
 
   return axios
-      .request(options)
-      .then((response) => {
-        res.json(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    .request(options)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error');
+    });
 });
